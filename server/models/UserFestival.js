cc5df1d6e8d405a75f9af5be337fbf16b2a8c60d
Hojun-Yoon <hojun9292@gmail.com>
2019-11-02 15:32:18 +0900

    variable updated

--- server/models/UserFestival.js
+++ server/models/UserFestival.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const UserFestival = sequelize.define('UserFestival', {
-    user_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true} ,
-    fest_id: DataTypes.INTEGER }, {});
+    user_Id: DataTypes.STRING,
+    festival_Id: DataTypes.INTEGER }, {});
     UserFestival.associate = function(models) {
     // associations can be defined here
   };
--- server/models/Users.js
+++ server/models/Users.js
@@ -2,9 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define('Users', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true} ,
+    user_Id: DataTypes.STRING,
     email: DataTypes.STRING,
     username: DataTypes.STRING,
-    photoUrl: DataTypes.STRING
+    givenname: DataTypes.STRING,
+    photourl: DataTypes.STRING
   }, {});
   Users.associate = function(models) {
     // associations can be defined here

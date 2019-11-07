a8793bbe4caad8a652d0802a4742cd0711720115
Hojun-Yoon <hojun9292@gmail.com>
2019-11-07 20:57:05 +0900

    file changed

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ test_1.on('connection', socket => {
     msg
       .create({
         user_Id: data.user._Id,
-        festival_Id: data.festival_Id,
+        //festival_Id: data.festival_Id,
         msg: data.text
       })
     test_1.emit('chat', {
